6c9fcd9d6a4dbf94672a7bf70f9bdce2304d1c04
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-22 11:04:36 +0530

    commit

---
+++ src/spotify.js
@@ -0,0 +1,7 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+
+const clientId = "ddf93795a7b34764a7038d1814bad227";
+
+const redirectUri = "http://localhost:3000/";
