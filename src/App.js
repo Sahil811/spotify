6c1b026da3b5d4ac0ce4c8c3e0b93ef60db45bd9
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-22 12:16:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Player from "./components/Player";
+import { getTokenFromResponse } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
